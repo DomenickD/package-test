7e6d756fbeac7038a60fc0ee50c30d7f3ed52869
DomenickD <deothe1@gmail.com>
2025-03-20 10:45:16 -0400

    7th commit

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="my_streamlit_app",
     version="0.1.0",
-    packages=find_packages(),  # Automatically finds all packages with __init__.py
+    packages=find_packages(
+        include=["my_streamlit_app", "my_streamlit_app.*"]
+    ),  # Ensures sub-packages are included
     install_requires=[
         "streamlit",
     ],
-    entry_points={
-        "console_scripts": [
-            "my-streamlit-app=my_streamlit_app.app:MyStreamlitApp",
-        ],
-    },
 )
