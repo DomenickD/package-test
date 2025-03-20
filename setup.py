45795522929c5a5332bdd68795d24c975caf3363
DomenickD <deothe1@gmail.com>
2025-03-20 08:57:29 -0400

    third commit

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="my_streamlit_app",
     version="0.1.0",
-    author="Domenick Dobbs",
-    author_email="Domenick.dobbs@gmail.com",
-    description="A Python package for a modular Streamlit app",
+    # author="Domenick Dobbs",
+    # author_email="Domenick.dobbs@gmail.com",
+    # description="A Python package for a modular Streamlit app",
     packages=find_packages(include=["page_handler", "page_handler.*"]),
     install_requires=[
         "streamlit",
